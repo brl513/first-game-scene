637dd12414cf2edc956bcd6bd47c5dcac813a6cb
brl513 <49693346+brl513@users.noreply.github.com>
2020-12-01 18:20:29 +0000

    Created script for radial movement and addeed it to sphere

---
+++ Moving Objects Scene/Assets/Scripts/RadialMotion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class RadialMotion : MonoBehaviour
+{
+    public Vector3 offset;
+
+    float timeCounter = 0;
+
+    float speed;
+    float width;
+    float depth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1;
+        width = 3;
+        depth = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime*speed;
+
+        float x = Mathf.Cos(timeCounter)*width;
+        float y = 0;
+        float z = Mathf.Sin(timeCounter)*depth;
+
+        transform.position = new Vector3(x, y, z) + offset;
+
+    }
+}
