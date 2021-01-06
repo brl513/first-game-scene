a97f6169e761a242bc973df8d44bbe0e86534dc9
brl513 <49693346+brl513@users.noreply.github.com>
2021-01-06 17:54:55 +0000

    Added scripts to read motion coordinates from JSON file, deleted OSC output.

---
+++ Moving Objects Scene/Assets/Scripts/InstantPosition.cs
@@ -0,0 +1,9 @@
+﻿[System.Serializable]
+public class InstantPosition
+{
+    //these variables are case sensitive and must match the strings "Time", "X", "Y", "Z" in the JSON.
+    public float time;
+    public float x;
+    public float y;
+    public float z;
+}
--- Moving Objects Scene/Assets/Scripts/RadialMotion.cs
+++ Moving Objects Scene/Assets/Scripts/RadialMotion.cs
@@ -4,13 +4,14 @@ public class RadialMotion : MonoBehaviour
 {
     public Vector3 offset;
 
+    public JSONReader jsonReader;
+
     float timeCounter = 0;
 
     public float speed;
     public float width;
     public float height;
     public float depth;
-    public bool rotationIsClockwise;
 
 
     // Start is called before the first frame update
@@ -22,20 +23,9 @@ public class RadialMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeCounter += Time.deltaTime*speed;
+        timeCounter += Time.deltaTime;
 
-        float x = Mathf.Cos(timeCounter) * width;
-        float y = Mathf.Sin(timeCounter) * height;
-        float z = Mathf.Sin(timeCounter) * depth;
-
-        if (rotationIsClockwise == true)
-        {
-            transform.position = new Vector3(-x, y, z) + offset;
-        }
-        else
-        {
-            transform.position = new Vector3(x, y, z) + offset;
-        }
+        transform.position = jsonReader.getPositionForTime(timeCounter) + offset;
 
     }
 }
